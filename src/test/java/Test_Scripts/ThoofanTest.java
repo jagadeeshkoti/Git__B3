4c58beeec4b8d5e36336595ce81e6e234c48b416
jagadeeshkoti <jagadeeshkoti96@gmail.com>
2023-09-30 17:06:32 +0530

    Done

--- src/test/java/Test_Scripts/ThoofanTest.java
+++ src/test/java/Test_Scripts/ThoofanTest.java
@@ -7,4 +7,18 @@ public class ThoofanTest {
 public void ttest() {
 	System.out.println("Destiny");
 }
+	
+	@Test
+	public void ttest1() {
+		System.out.println("Reached");
+	}
+		
+		
+		@Test
+		public void ttest2() {
+			System.out.println("Reached Done");
+		
+	
+	
+}
 }
